d14e6e13608063186598df3e79419a8d5bec19a2
Juwon-Yun <eooll2@daum.net>
2022-06-05 15:27:05 +0900

    Feat: gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomeScreen(),
   ));
 }
@@ -12,7 +13,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: Container(
+      // BoxDecoration안에 color로 배경색을 바꾸는게 정석
+      // BoxDecoration에 color값을 주면 Container의 Color는 쓰지 말아야함
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                // begin
+                Color(0xFF2A3A7C),
+                Color(0xFF000118),
+              ]
+          )
+      ),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('asset/img/logo.png'),
+          Row(
+            children: [const Text('Video'), const Text('PLAYER')],
+          )
+        ],
+      ),
+    ));
   }
 }
